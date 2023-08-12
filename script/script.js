252e9813ebb32add7c3448af411f5bfcc88574b9
dsatpm <jeremiahjh@gmail.com>
2023-08-11 22:49:12 -0700

    Changed timer to 20 seconds for game length and 'prettified' the organization, left comments, etc.

--- script/script.js
+++ script/script.js
@@ -7,13 +7,34 @@ const timeEl = document.getElementById('cdTimer');
 // Game variables
 let gameIsRunning = false;
 let untilStart;
-let sixtySeconds = 60;
+let secondsLeft = 20;
 const possibleWordList = [
-  'javascript', 'programming', 'queryselector', 'element', 'getelementbyid',
-  'array', 'iterating', 'object', 'method', 'function', 'loop', 'setinterval',
-  'document', 'innerhtml', 'addeventlistener', 'textcontent', 'string',
-  'integer', 'boolean', 'dom', 'setid', 'getid', 'localstorage', 'click',
-  'keydown', 'event'
+	'javascript',
+	'programming',
+	'queryselector',
+	'element',
+	'getelementbyid',
+	'array',
+	'iterating',
+	'object',
+	'method',
+	'function',
+	'loop',
+	'setinterval',
+	'document',
+	'innerhtml',
+	'addeventlistener',
+	'textcontent',
+	'string',
+	'integer',
+	'boolean',
+	'dom',
+	'setid',
+	'getid',
+	'localstorage',
+	'click',
+	'keydown',
+	'event',
 ];
 let possibleWord;
 let wrongGuess = 0;
@@ -26,116 +47,117 @@ let startGameInterval;
 
 // Function to start the game
 function gameStart() {
-  clearInterval(startGameInterval);
-  startGameInterval = setInterval(function () {
-    untilStart--;
+	clearInterval(startGameInterval);
+	startGameInterval = setInterval(function () {
+		untilStart--;
 
-    if (untilStart > 0) {
-      timeEl.textContent = untilStart;
-      timeEl.style.opacity = 1;
-      setTimeout(function () {
-        timeEl.style.opacity = 0;
-      }, 500);
-    } else {
-      timeEl.textContent = '';
-      clearInterval(startGameInterval);
+		if (untilStart > 0) {
+			timeEl.textContent = untilStart;
+			timeEl.style.opacity = 1;
+			setTimeout(function () {
+				timeEl.style.opacity = 0;
+			}, 500);
+		} else {
+			timeEl.textContent = '';
+			clearInterval(startGameInterval);
 
-      wordPicker();
-      gameTimer();
-    }
-  }, 1000);
+			wordPicker();
+			gameTimer();
+		}
+	}, 1000);
 }
 
 // Function to pick a random word
 function wordPicker() {
-  possibleWord = possibleWordList[Math.floor(Math.random() * possibleWordList.length)];
-  const underScore = '_ '.repeat(possibleWord.length);
-  wordDisplayEl.textContent = underScore;
+	possibleWord =
+		possibleWordList[Math.floor(Math.random() * possibleWordList.length)];
+	const underScore = '_ '.repeat(possibleWord.length);
+	wordDisplayEl.textContent = underScore;
 }
 
 // Function to handle game timer
 function gameTimer() {
-  clearInterval(startClockInterval);
-  startClockInterval = setInterval(function () {
-    sixtySeconds--;
+	clearInterval(startClockInterval);
+	startClockInterval = setInterval(function () {
+		secondsLeft--;
 
-    if (sixtySeconds >= 0) {
-      gameTimerEl.textContent = sixtySeconds;
+		if (secondsLeft >= 0) {
+			gameTimerEl.textContent = secondsLeft;
 
-    if (sixtySeconds <= 10) {
-      gameTimerEl.style.color = 'orangered';
-      gameTimerEl.style.fontSize = '8rem';
-      gameTimerEl.style.width = '550px';
-      gameTimerEl.textContent += ' - HURRY UP!';
-    } else {
-      gameTimerEl.style.color = 'whitesmoke';
-    } 
-  } else {
-    gameTimerEl.textContent = '';
-    clearInterval(startClockInterval);
+			if (secondsLeft <= 5) {
+				gameTimerEl.style.color = 'orangered';
+				gameTimerEl.style.fontSize = '8rem';
+				gameTimerEl.style.width = '500px';
+				gameTimerEl.textContent += ' - HURRY UP!';
+			} else {
+				gameTimerEl.style.color = 'whitesmoke';
+			}
+		} else {
+			gameTimerEl.textContent = '';
+			clearInterval(startClockInterval);
 
-    score--;
-    endGame();
+			score--;
+			endGame();
 
-    wordDisplayEl.innerHTML = `YOU SUCK LOSER.<br>Score: ${score}`;
-  }
-  }, 1000);
+			wordDisplayEl.innerHTML = `YOU SUCK LOSER.<br>Score: ${score}`;
+		}
+	}, 1000);
 }
 
+// Function ends current game
 function endGame() {
-  clearInterval(startGameInterval);
-  clearInterval(startClockInterval);
-  gameIsRunning = false;
-  gameState.classList.remove('flip');
-  wordDisplayEl.textContent = '';
+	clearInterval(startGameInterval);
+	clearInterval(startClockInterval);
+	gameIsRunning = false;
+	gameState.classList.remove('flip');
+	wordDisplayEl.textContent = '';
 }
 
 // Function to update the word display
 function updateDisplay() {
-  wordDisplayEl.textContent = wordDisplay.join(' ');
+	wordDisplayEl.textContent = wordDisplay.join(' ');
 }
 
 // Event listener for game state button
 gameState.addEventListener('click', function () {
-  if (gameIsRunning) {
-    endGame();
-    wordDisplayEl.textContent = 'GAME OVER!';
-  } else {
-    endGame();
-    untilStart = 4;
-    sixtySeconds = 60;
-    gameStart();
-    gameIsRunning = true;
-  }
+	if (gameIsRunning) {
+		endGame();
+		wordDisplayEl.textContent = 'GAME OVER!';
+	} else {
+		endGame();
+		untilStart = 4;
+		secondsLeft = 20;
+		gameStart();
+		gameIsRunning = true;
+	}
 
-  gameState.classList.toggle('flip');
+	gameState.classList.toggle('flip');
 });
 
 // Event listener for keyboard input
 document.addEventListener('keydown', function (event) {
-  if (gameIsRunning) {
-    const key = event.key.toLowerCase();
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	if (gameIsRunning) {
+		const key = event.key.toLowerCase();
+		const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-    if (alphabet.includes(key)) {
-      if (possibleWord.includes(key)) {
-        wordDisplay = wordDisplayEl.textContent.split(' ');
+		if (alphabet.includes(key)) {
+			if (possibleWord.includes(key)) {
+				wordDisplay = wordDisplayEl.textContent.split(' ');
 
-        possibleWord.split('').forEach((letter, i) => {
-          if (letter === key) {
-            wordDisplay[i] = key;
-          }
-        });
+				possibleWord.split('').forEach((letter, i) => {
+					if (letter === key) {
+						wordDisplay[i] = key;
+					}
+				});
 
-        updateDisplay();
+				updateDisplay();
 
-        if (wordDisplay.join('') === possibleWord) {
-          score++;
-          endGame();
-          wordDisplayEl.innerHTML = `HELL YEAH, BROTHER! YOU GUESSED ${possibleWord}! <br>Score: ${score}`;
-        }
-        
-      }
-    }
-  }
+				if (wordDisplay.join('') === possibleWord) {
+					score++;
+					endGame();
+					wordDisplayEl.innerHTML = `HELL YEAH, BROTHER! YOU GUESSED ${possibleWord}! <br>Score: ${score}`;
+				}
+			}
+		}
+	}
 });
